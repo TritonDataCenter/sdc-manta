TritonDataCenter/manta-mako

MANTA-4776 change manta image names mantav1-* on "mantav1" branch

Makefile
@@ -54,7 +54,7 @@ NODE_PREBUILT_IMAGE	= 1ad363ec-3b83-11e8-8521-2f68a4a34d5d
 # Stuff used for buildimage
 #
 BASE_IMAGE_UUID		= b6ea7cb4-6b90-48c0-99e7-1d34c2895248
-BUILDIMAGE_NAME		= manta-storage
+BUILDIMAGE_NAME		= mantav1-storage
 BUILDIMAGE_DESC		= Manta Storage
 BUILDIMAGE_PKGSRC	= pcre-8.42 findutils-4.6.0nb1 gawk-4.1.4nb1
 AGENTS = amon config minnow registrar

TritonDataCenter/sdc-manta

Makefile
@@ -92,7 +92,7 @@ NAME		= manta-deployment
 RELEASE_TARBALL := $(NAME)-pkg-$(STAMP).tar.gz
 
 BASE_IMAGE_UUID = 04a48d7d-6bb5-4e83-8c3b-e60a99e0f48f
-BUILDIMAGE_NAME = $(NAME)
+BUILDIMAGE_NAME = mantav1-deployment
 BUILDIMAGE_DESC	= Manta deployment tools
 BUILDIMAGE_PKGSRC = openldap-client-2.4.44nb2
 AGENTS		= amon config